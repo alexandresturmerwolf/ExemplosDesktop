9b3b9357eb7298288426993dbc08302f946d1064
wolfi <wolfi@DESKTOP-9ACL808>
2020-05-08 20:46:33 -0300

    Commit inicial

---
+++ src/exemplosdesktop/ExemplosDesktop.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplosdesktop;
+
+/**
+ *
+ * @author wolfi
+ */
+public class ExemplosDesktop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("A vaca morreu");
+    }
+    
+}
