36b25479e300a1f801582e7b4c424c309feaa99f
wolfi <wolfi@DESKTOP-9ACL808>
2020-05-22 20:44:46 -0300

    Initial version

--- src/exemplosdesktop/ExemplosDesktop.java
+++ src/exemplosdesktop/ExemplosDesktop.java
@@ -3,8 +3,8 @@ package exemplosdesktop;
 public class ExemplosDesktop {
 
     public static void main(String[] args) {
-        TelaPrincipal tela = new TelaPrincipal();
-        tela.setVisible(true);
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.setVisible(true);
     }
 
 }
